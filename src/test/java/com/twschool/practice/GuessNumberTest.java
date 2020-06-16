86f3c0b5261459e9bb5c37f8673568bbadb0fbdf
huyuanixn325 <huyuanxin0325@163.com>
2020-06-16 21:19:36 +0800

    should_return_0A4B_when_given_1234_and_4321

--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -9,7 +9,7 @@ public class GuessNumberTest {
     Converse converse = new Converse();
     GuessNumber guessNumber = new GuessNumber();
     @Test
-    public void should_return_1A3B_when_input_1234_and_1423(){
+    public void should_return_2A2B_when_given_1234_and_1423(){
         //given
         String userAnswer = "1 4 3 2";
         List<String> userAnswerList = converse.converse(userAnswer);
@@ -18,5 +18,29 @@ public class GuessNumberTest {
         String answerResult = guessNumber.count(userAnswerList,gameAnswer);
         //then
         Assert.assertEquals("2A2B",answerResult);
+    }
+
+    @Test
+    public void should_return_4A0B_when_given_1234_and_1234(){
+        //given
+        String userAnswer = "1 2 3 4";
+        List<String> userAnswerList = converse.converse(userAnswer);
+        String gameAnswer = "1234";
+        //when
+        String answerResult = guessNumber.count(userAnswerList,gameAnswer);
+        //then
+        Assert.assertEquals("4A0B",answerResult);
+    }
+
+    @Test
+    public void should_return_0A4B_when_given_1234_and_4321(){
+        //given
+        String userAnswer = "1 2 3 4";
+        List<String> userAnswerList = converse.converse(userAnswer);
+        String gameAnswer = "4321";
+        //when
+        String answerResult = guessNumber.count(userAnswerList,gameAnswer);
+        //then
+        Assert.assertEquals("0A4B",answerResult);
     }
 }
