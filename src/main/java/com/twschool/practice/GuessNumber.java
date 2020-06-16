fb5a4319a9e0792cbde53d7ddc37034cb0ce260c
huyuanixn325 <huyuanxin0325@163.com>
2020-06-16 21:14:55 +0800

    finished should_return_1A3B_when_input_1234_and_1423

--- src/main/java/com/twschool/practice/GuessNumber.java
+++ src/main/java/com/twschool/practice/GuessNumber.java
@@ -3,5 +3,35 @@ package com.twschool.practice;
 import java.util.List;
 
 public class GuessNumber {
-   
+    public String count(List<String> userAnswerList,String gameAnswer) {
+        int sameCount = countSameNumber(userAnswerList,gameAnswer);
+        int sameSpaceNumber=countSameSpanceNumber(userAnswerList,gameAnswer);
+        return formatOutPut(sameCount,sameSpaceNumber);
+    }
+
+    public int countSameNumber(List<String> guestNumberList,String gameAnswer){
+        int sameCount = 0;
+        //System.out.println(answer.contains(guestNumberList.get(2)));
+        for(String s:guestNumberList) {
+            if(gameAnswer.contains(s)) {
+                sameCount ++;
+            }
+        }
+        return sameCount;
+
+    }
+
+    public int countSameSpanceNumber(List<String> guessNumberList,String gameAnswer){
+        int sameSpaceNumber = 0;
+        for(int i = 0;i<gameAnswer.length();i++){
+            if (String.valueOf(gameAnswer.charAt(i)).equals(guessNumberList.get(i))){
+                sameSpaceNumber++;
+            }
+        }
+        return sameSpaceNumber;
+    }
+
+    public String formatOutPut(int sameCount,int sameSpaceNumber){
+        return sameSpaceNumber+"A"+(sameCount-sameSpaceNumber)+"B";
+    }
 }
--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -11,6 +11,12 @@ public class GuessNumberTest {
     @Test
     public void should_return_1A3B_when_input_1234_and_1423(){
         //given
-
+        String userAnswer = "1 4 3 2";
+        List<String> userAnswerList = converse.converse(userAnswer);
+        String gameAnswer = "1234";
+        //when
+        String answerResult = guessNumber.count(userAnswerList,gameAnswer);
+        //then
+        Assert.assertEquals("2A2B",answerResult);
     }
 }
