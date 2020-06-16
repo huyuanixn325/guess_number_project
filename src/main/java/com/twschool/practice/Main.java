36fb560ffe56f46583a6a611744ac4da4c8c8335
huyuanixn325 <huyuanxin0325@163.com>
2020-06-16 22:00:03 +0800

    add the history

--- src/main/java/com/twschool/practice/Main.java
+++ src/main/java/com/twschool/practice/Main.java
@@ -5,12 +5,17 @@ public class Main {
     public static void main(String[] args) throws Exception {
         GenerateNumber generateNumber = new GenerateNumber();
         String answer = generateNumber.generateNumber(4);
+        String history = "";
         Game game = new Game();
         for (int i = 0; i < RUN_TIMES; i++) {
+            if (i==0) history="";
             String input = new GuessInputCommand().input();
+            history=history+input+"\n";
             System.out.println(input);
             String result = game.game(answer,input);
             System.out.println(result);
+            System.out.println("你输入的历史纪录：\n"+history);
+            if (result.contains("4A0B")) break;
         }
     }
 }
